906a7d62375bea66c709ab2579c5119e80753bc5
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-28 18:39:28 +0600

    featured project api connected

--- src/app/page.js
+++ src/app/page.js
@@ -10,27 +10,31 @@ async function getData() {
     const res2 = await fetch(`${baseURL}/api/BrandList`);
     const res3 = await fetch(`${baseURL}/api/WorkList`);
     const res4 = await fetch(`${baseURL}/api/StatList`);
+    const res5 = await fetch(`${baseURL}/api/FeaturedProject`);
     const heroDataRes = await res1.json();
     const brandListRes = await res2.json();
     const workListRes = await res3.json();
     const statListRes = await res4.json();
+    const featuredProjectRes = await res5.json();
 
     return {
         heroData: heroDataRes,
         brandList: brandListRes,
         workList: workListRes,
         statList: statListRes,
+        featuredProjectData: featuredProjectRes,
     };
 }
 
 export default async function Home() {
-    const { heroData, brandList, workList, statList } = await getData();
+    const { heroData, brandList, workList, statList, featuredProjectData } =
+        await getData();
     return (
         <main>
             <Banner data={heroData} brandList={brandList} />
             <WorkList data={workList} />
             <StatList data={statList} />
-            <FeaturedProject />
+            <FeaturedProject data={featuredProjectData} />
             <Subscribe />
         </main>
     );
--- src/components/sections/features-project.js
+++ src/components/sections/features-project.js
@@ -1,50 +1,8 @@
 import moment from "moment/moment";
 import Image from "next/image";
-import img1 from "../../../public/featured-project/1.png";
-import img2 from "../../../public/featured-project/2.png";
-import img3 from "../../../public/featured-project/3.png";
-import img4 from "../../../public/featured-project/4.png";
-import img5 from "../../../public/featured-project/5.png";
 import SectionTitle from "./section-title";
 
-const FeaturedProject = () => {
-    const data = [
-        {
-            id: 1,
-            title: "App Redesign",
-            image: img1,
-            remark: "App Design",
-            createdAt: "2023-09-20T05:29:26.000000Z",
-        },
-        {
-            id: 2,
-            title: "Redesign channel website landng page",
-            image: img2,
-            remark: "App Design",
-            createdAt: "2023-09-20T05:29:26.000000Z",
-        },
-        {
-            id: 3,
-            title: "Redesign channel website landng page",
-            image: img3,
-            remark: "App Design",
-            createdAt: "2023-09-20T05:29:26.000000Z",
-        },
-        {
-            id: 4,
-            title: "Redesign channel website landng page",
-            image: img4,
-            remark: "App Design",
-            createdAt: "2023-09-20T05:29:26.000000Z",
-        },
-        {
-            id: 5,
-            title: "Redesign channel website landng page",
-            image: img5,
-            remark: "App Design",
-            createdAt: "2023-09-20T05:29:26.000000Z",
-        },
-    ];
+const FeaturedProject = ({ data }) => {
     return (
         <div className="py-[61px] pb-[84px] bg-lightGreen">
             <section className="section h-auto">
@@ -64,24 +22,24 @@ const FeaturedProject = () => {
                         </div>
                         <p className="text-[14px] text-[#2E3E5C] font-normal ">
                             {data[0]["remark"]} -{" "}
-                            {moment(data[0]["createdAt"]).format("LL")}
+                            {moment(data[0]["created_at"]).format("LL")}
                         </p>
                         <h4>{data[0]["title"]}</h4>
                     </div>
                     {data
                         .slice(1, 5)
-                        .map(({ id, image, title, remark, createdAt }) => (
+                        .map(({ id, image, title, remark, created_at }) => (
                             <div key={id} className="relative space-y-[15px]">
                                 <div className="relative min-h-[180px]">
                                     <Image
                                         src={image}
                                         fill
-                                        className="min-h-[188px]"
+                                        className="min-h-[188px] rounded-[20px] "
                                         alt="project"
                                     />
                                 </div>
                                 <p className="text-[14px] text-[#2E3E5C] font-normal ">
-                                    {remark} - {moment(createdAt).format("LL")}
+                                    {remark} - {moment(created_at).format("LL")}
                                 </p>
                                 <h4>{title}</h4>
                             </div>
