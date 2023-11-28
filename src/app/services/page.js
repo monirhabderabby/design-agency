86868fc3fc8a820c4939535e74a9eb47a54bbfb7
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-28 18:55:47 +0600

    api intregated for services data

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -2,11 +2,22 @@ import AllServices from "@/components/sections/all-services";
 import Subscribe from "@/components/sections/subscribe";
 import PageHeader from "@/components/shared/header/page-header";
 
-const Services = () => {
+async function getData() {
+    const baseURL = process.env.baseURL;
+    const res1 = await fetch(`${baseURL}/api/AllService`);
+    const data = await res1.json();
+
+    return {
+        servicesData: data,
+    };
+}
+
+const Services = async () => {
+    const { servicesData } = await getData();
     return (
         <div>
             <PageHeader currentPage="Our Services" />
-            <AllServices />
+            <AllServices data={servicesData} />
             <Subscribe />
         </div>
     );
